89e0b45b17da019d7468c42d159ff31d0f94b844
IVONIX <infosalechara@gmail.com>
2026-09-10 00:36:55 +0400

    feat: add ontology application bootstrap

---
+++ apps/ontology/src/main/java/com/ivonix/pulse/ontology/PulseOntologyApplication.java
@@ -0,0 +1,11 @@
+package com.ivonix.pulse.ontology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseOntologyApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PulseOntologyApplication.class, args);
+  }
+}
