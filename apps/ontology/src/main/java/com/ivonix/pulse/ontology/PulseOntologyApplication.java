f6f12dc47a84d7bf6614c5f737f24e991bdb424d
IVONIX <infosalechara@gmail.com>
2026-09-10 00:44:53 +0400

    feat: enable scheduled security maintenance

--- apps/ontology/src/main/java/com/ivonix/pulse/ontology/PulseOntologyApplication.java
+++ apps/ontology/src/main/java/com/ivonix/pulse/ontology/PulseOntologyApplication.java
@@ -2,10 +2,10 @@ package com.ivonix.pulse.ontology;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PulseOntologyApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(PulseOntologyApplication.class, args);
-  }
+  public static void main(String[] args) { SpringApplication.run(PulseOntologyApplication.class,args); }
 }
